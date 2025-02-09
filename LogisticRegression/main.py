a83fbac345f57b472cc5d19e3367de0440750c18
eram97 <eramfatima180@gmail.com>
2025-02-09 16:53:08 +0530

    Update main.py

--- LogisticRegression/main.py
+++ LogisticRegression/main.py
@@ -3,3 +3,5 @@ import numpy as np
 
 print (f'The pandas version is.{pd.__version__}')
 print (f'The numpy version is.{np.__version__}')
+
+print ('pandas is the best library for data manipulation in python')
