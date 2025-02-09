cb2b0fe2daac6a253b17b07994b5c5df01840ada
Eram Fatima <eramfatima180@gmail.com>
2025-02-09 17:01:41 +0530

    removed main

--- LogisticRegression/main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-print (f'The pandas version is.{pd.__version__}')
-print (f'The numpy version is.{np.__version__}')
-
-print ('pandas is the best library for data manipulation in python')
-
-print ('numpy is the best library numerical computation in python')
